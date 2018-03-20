0e89a0ea2be203f891aea8a8e8f97a2145a746fd
Kyle Hunt <Kyle_Hunt@ymail.com>
2018-03-19 21:29:55 -0500

    Version

--- Avabur_Enhancer.user.js
+++ Avabur_Enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Avabur Enhancer
 // @namespace    https://github.com/sobfiggis/Avabur_Enhancer
-// @version      0.9.6
+// @version      1.0
 // @description  Tracks certain data within the game to create additional features and calculate additional informaiton.
 // @author       Original Creator: Kajin. Contributors: Kaymo, WinterPheonix
 // @match        https://avabur.com/game
