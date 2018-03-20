0b12c71fbd2ad2666559412e7cd7695e89c2a16a
Kyle Hunt <Kyle_Hunt@ymail.com>
2018-03-19 21:28:03 -0500

    Webhook

--- Avabur_Enhancer.user.js
+++ Avabur_Enhancer.user.js
@@ -3,13 +3,99 @@
 // @namespace    https://github.com/sobfiggis/Avabur_Enhancer
 // @version      0.9.6
 // @description  Tracks certain data within the game to create additional features and calculate additional informaiton.
-// @author       In Game Name: Kajin
+// @author       Original Creator: Kajin. Contributors: WinterPheonix
 // @match        https://avabur.com/game
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.js
-// @require      https://github.com/edvordo/RoA-WSHookUp/raw/master/RoA-WSHookUp.user.js
 // @downloadURL  https://raw.githubusercontent.com/sobfiggis/Avabur_Enhancer/master/Avabur_Enhancer.user.js
 // ==/UserScript==
+
+
+/*
+ * ROA WebSocket Hook Kindly provided by edvordo see:
+ * https://github.com/edvordo/RoA-WSHookUp/ 
+ * this can also be used to clean up a lot of this code..
+*/
+(function(window) {
+    'use strict';
+
+    var OriginalWS = window.WebSocket;
+    var initOWS    = OriginalWS.apply.bind(OriginalWS);
+    var wsListener = OriginalWS.prototype.addEventListener;
+        wsListener = wsListener.call.bind(wsListener);
+    var hooked = false;
+    function MyWS(url, opts) {
+        var ws;
+        if (this instanceof WebSocket) {
+            if (arguments.length === 1) {
+                ws = new OriginalWS(url);
+            } else if (arguments.length >= 2) {
+                ws = new OriginalWS();
+            } else {
+                ws = new OriginalWS();
+            }
+        } else {
+            ws = initOWS(this, arguments);
+        }
+        wsListener(ws, "message", function(event){
+            hooked = true;
+            var etype = "roa-ws:";
+            var data;
+            try {
+                data = JSON.parse(event.data);
+                for (var i = 0; i < data.length; i++) {
+                    etype = "roa-ws:";
+                    var etypepage = "";
+                    var item = data[i];
+                    if (item.hasOwnProperty("type")) {
+                        etype = etype + item.type;
+                        // in case its a "page" type message create additional event
+                        // e.g.: "roa-ws:page:boosts", "roa-ws:page:clans" or "roa-ws:page:settings_milestones" etc.
+                        if (item.type === "page" && item.hasOwnProperty("page") && "string" === typeof item.page) {
+                            etypepage = etype + ":" + item.page;
+                        }
+                    } else {
+                        etype = etype + "general";
+                    }
+
+                    // This is a special kind of message containing info about the user
+                    // I'm resending it in 5 second because this script is supposed to
+                    // run at "document start" and potential userscripts that are
+                    // listening to the even "roa:login_info" may not catch this message.
+                    // They should, I got the message 99 times out of 100, which shows
+                    // that userscripts are not 100% reliable with their running queue.
+                    if (etype === "roa-ws:login_info") {
+                        setTimeout(function(_etype, _item){
+                            $(document).trigger(_etype, _item);
+                        }, 5000, etype, item);
+                    }
+
+                    $(document).trigger(etype, item);
+                    if (etypepage.length > 0) {
+                        $(document).trigger(etypepage, item);
+                    }
+                }
+            } catch (ex) {
+                data = event.data;
+                etype = etype + "general";
+                $(document).trigger(etype, data);
+            }
+
+            $(document).trigger("roa-ws:all", event.data);
+        });
+        return ws;
+    }
+    window.WebSocket = MyWS.bind();
+    window.WebSocket.prototype = OriginalWS.prototype;
+    window.WebSocket.prototype.constructor = window.WebSocket;
+
+    setTimeout(function(){
+        if (hooked === false) {
+            $.alert("The script 'WSHookUp' failed to hook to the WebSocket connection Avabur uses. In case you are using another userscript that depends on this, you may want to refresh the page to try again.", "WSHookUp Failed");
+        }
+    }, 30000);
+})(window);
+
 
 /**************************************************/
 /****************** USER OPTIONS ******************/
